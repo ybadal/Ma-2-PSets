f9ae1f8ab39e2dfb3770bce7e5cdd0b4932ba6ca
Yovan Badal <yovanbadal@gmail.com>
2017-11-12 17:07:53 -0800

    Creating Ma2 repo

---
+++ PS6/numerical_integration.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+
+def func(y):
+	"""Defining the function F for our differential equation"""
+	return float(3*y - 2)
+
+
+# We start with Euler's Method:
+def euler(func, x0, y0, N, x):
+	"""Using the Euler method to numerically approximate a solution to our equation with F func,
+	intial conditions x0 and y0, N steps and final x value x"""
+	h = float((x-x0)/float(N))
+	x_arr = np.linspace(x0, x, num=N+1)
+	y_arr = np.linspace(x0, x, num=N+1)
+	y_arr[0] = y0
+	y_ac = np.linspace(x0, x, num=N+1)
+	y_ac[0] = y0
+	err = np.linspace(x0, x, num=N+1)
+	err[0] = 0
+	xy_arr = np.zeros((N+1, 4))
+	xy_arr[0, 0], xy_arr[0, 1], xy_arr[0,2], xy_arr[0, 3] = x0, y0, y0, 0
+
+	for j in range(N+1)[1:]:
+		y_arr[j] = y_arr[j-1] + float(h)*float(func(y_arr[j-1]))
+		y_ac[j] = float((np.e**(3*x_arr[j]) + 2)/3)
+		err[j] = float(y_ac[j] - y_arr[j])
+		xy_arr[j, 0], xy_arr[j, 1], xy_arr[j, 2], xy_arr[j, 3] 	= x_arr[j], y_arr[j], y_ac[j], err[j]
+
+	return xy_arr
+
+
+# We now use the Euler Trapezoid method
+def euler_trap(func, x0, y0, N, x):
+	"""Using the Euler trapezoid method to numerically approximate a solution to our equation with F func,
+	intial conditions x0 and y0, N steps and final x value x"""
+	h = float((x-x0)/float(N))
+	x_arr = np.linspace(x0, x, num=N+1)
+	y_arr = np.linspace(x0, x, num=N+1)
+	y_arr[0] = y0
+	y_ac = np.linspace(x0, x, num=N+1)
+	y_ac[0] = y0
+	err = np.linspace(x0, x, num=N+1)
+	err[0] = 0
+	xy_arr = np.zeros((N+1, 4))
+	xy_arr[0, 0], xy_arr[0, 1], xy_arr[0,2], xy_arr[0, 3] = x0, y0, y0, 0
+
+	for j in range(N+1)[1:]:
+		yj1 = y_arr[j-1] + float(h)*float(func(y_arr[j-1]))
+		y_arr[j] = y_arr[j-1] + float(h/2)*(float(func(y_arr[j-1])) + float(func(yj1)))
+		y_ac[j] = float((np.e**(3*x_arr[j]) + 2)/3)
+		err[j] = float(y_ac[j] - y_arr[j])
+		xy_arr[j, 0], xy_arr[j, 1], xy_arr[j, 2], xy_arr[j, 3] 	= x_arr[j], y_arr[j], y_ac[j], err[j]
+	
+	return xy_arr
+
+
+# We now use the Runge-Kutta method
+def runge_kutta(func, x0, y0, N, x):
+	"""Using the Runge-Kutta method to numerically approximate a solution to our equation with F func,
+	intial conditions x0 and y0, N steps and final x value x"""
+	h = float((x-x0)/float(N))
+	x_arr = np.linspace(x0, x, num=N+1)
+	y_arr = np.linspace(x0, x, num=N+1)
+	y_arr[0] = y0
+	y_ac = np.linspace(x0, x, num=N+1)
+	y_ac[0] = y0
+	err = np.linspace(x0, x, num=N+1)
+	err[0] = 0
+	xy_arr = np.zeros((N+1, 4))
+	xy_arr[0, 0], xy_arr[0, 1], xy_arr[0,2], xy_arr[0, 3] = x0, y0, y0, 0
+
+	for j in range(N+1)[1:]:
+                kj1 = float(func(y_arr[j-1]))
+                kj2 = float(func(y_arr[j-1] + float(h/2)*kj1))
+                kj3 = float(func(y_arr[j-1] + float(h/2)*kj2))
+                kj4 = float(func(y_arr[j-1] + float(h)*kj3))
+                y_arr[j] = y_arr[j-1] + float(h)*((kj1 + 2*kj2 + 2*kj3 + kj4)/6)
+		y_ac[j] = float((np.e**(3*x_arr[j]) + 2)/3)
+		err[j] = float(y_ac[j] - y_arr[j])
+		xy_arr[j, 0], xy_arr[j, 1], xy_arr[j, 2], xy_arr[j, 3] 	= x_arr[j], y_arr[j], y_ac[j], err[j]
+
+        return xy_arr
+
+
+print(euler(func, 0, 1, 200, 10)[200])
+print(euler_trap(func, 0, 1, 200, 10)[200])
+print(runge_kutta(func, 0, 1, 200, 10)[200])
+
